f2797aff98708a840de328f3c02fa07e1aa7b2e6
trangtanduoc <trangtanduoc@gmail.com>
2025-06-12 08:17:56 +0700

    update map3

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -4,7 +4,7 @@ public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] protected float damage;
 
-    protected void OntriggerEnter2D(Collider2D collision)
+    protected void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
             collision.GetComponent<Health>().TakeDamage(damage);
--- Assets/Scripts/ShottingTrap.cs
+++ Assets/Scripts/ShottingTrap.cs
@@ -1,37 +1,40 @@
 using UnityEngine;
 
-public class ArrowTrap : MonoBehaviour
+public class ShottingTrap : MonoBehaviour
 {
-    [SerializeField] private float attackCooldown;
+    [SerializeField] private float attackCooldown = 2f;
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject[] arrows;
+    [SerializeField] private Vector2 shootDirection = Vector2.right; // Default (1,0)
+
     private float cooldownTimer;
 
     private void Attack()
     {
         cooldownTimer = 0;
-        arrows[FindArrows()].transform.position = firePoint.position;
-        arrows[FindArrows()].GetComponent<EnemyProjectile>().ActiveProjectile();
+        int arrowIndex = FindArrows();
+        GameObject arrow = arrows[arrowIndex];
+        arrow.transform.position = firePoint.position;
+
+        ArrowCollision projectile = arrow.GetComponent<ArrowCollision>();
+        projectile.SetDirection(shootDirection);
+        projectile.ActiveProjectile();
     }
+
     private int FindArrows()
     {
         for (int i = 0; i < arrows.Length; i++)
         {
             if (!arrows[i].activeInHierarchy)
-            {
                 return i;
-            }
         }
         return 0;
-
     }
+
     private void Update()
     {
         cooldownTimer += Time.deltaTime;
         if (cooldownTimer >= attackCooldown)
-        {
             Attack();
-        }
     }
-
 }
